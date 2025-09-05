8f6f7734c37a1a2362948397fa07bc289c4d31fa
sanyathecreator <olexxxandr.romaniuk@gmail.com>
2025-09-05 19:47:09 +0200

    Set up the main controller and test it with dummy data

--- src/main/java/com/sanyathecreator/todoapp/controller/TodoController.java
+++ src/main/java/com/sanyathecreator/todoapp/controller/TodoController.java
@@ -2,12 +2,45 @@ package com.sanyathecreator.todoapp.controller;
 
 import io.github.palexdev.materialfx.controls.MFXComboBox;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+import java.time.LocalDateTime;
+
 public class TodoController {
+    @FXML
     public MFXComboBox statusComboBox;
+    @FXML
     public VBox taskList;
 
-    public void handleAddTask(ActionEvent actionEvent) {
+    public void initialize() {
+        statusComboBox.getItems().addAll("All", "ToDo", "In Progress", "Done");
+        statusComboBox.setValue("All");
+        // Dummy data
+        addTask("Create a JavaFX Project", LocalDateTime.now().minusMinutes(3), "ToDo");
+        addTask("Learn React", LocalDateTime.now().minusMinutes(9), "In Progress");
+        addTask("Create a TodoApp", LocalDateTime.now().minusHours(3).minusMinutes(17), "ToDo");
     }
+
+    @FXML
+    public void handleAddTask(ActionEvent actionEvent) {
+        addTask("New Task", LocalDateTime.now(), "Todo");
+    }
+
+    private void addTask(String name, LocalDateTime timeStamp, String status) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass()
+                    .getResource("/com/sanyathecreator/todoapp/task-card.fxml"));
+            HBox taskCard = loader.load();
+            TaskCardController taskCardController = loader.getController();
+            taskCardController.setTaskDetails(name, timeStamp, status);
+
+            taskList.getChildren().add(taskCard);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
